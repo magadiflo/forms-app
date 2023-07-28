8d7d1dc5f398dfe00eccd121c57cf9538d930f7d
Martín <magadiflo@gmail.com>
2023-07-28 18:08:08 -0500

    Formulario de Registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this._fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    'password-confirm': ['', [Validators.required]],
+  });
+
+  constructor(private _fb: FormBuilder) { }
+
+  onSave(): void {
+    this.myForm.markAllAsTouched();
+  }
+
+  isInvalidField(field: string) {
+    // TODO: Obtener validación desde un servicio
+  }
 
 }
