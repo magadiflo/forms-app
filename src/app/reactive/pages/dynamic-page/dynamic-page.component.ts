89b9326b7f65aae3ad0e614e767f5a9b6789cf34
Martín <magadiflo@gmail.com>
2023-07-28 11:34:31 -0500

    Formularios dinámicos

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicPageComponent {
+
+  public myForm: FormGroup = this._fb.nonNullable.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this._fb.array([ //* <-- Este sí es un arreglo
+      ['Fifa 2008', Validators.required], //* <-- Es un formControl pero en versión FormBuilder
+      ['Crash Car', Validators.required]
+    ]),
+  });
+
+  constructor(private _fb: FormBuilder) { }
+
+  public get favoriteGames() {
+    return this.myForm.controls['favoriteGames'] as FormArray;
+  }
+
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
