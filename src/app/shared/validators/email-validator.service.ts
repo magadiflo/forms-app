71e1e1fef4bc06b6d3ec981990526c058ea08fe4
Martín <magadiflo@gmail.com>
2023-07-31 10:41:04 -0500

    Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -9,9 +9,31 @@ export class EmailValidatorService implements AsyncValidator {
 
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log(email);
-
-    return of({ emailTaken: true }).pipe(delay(5000));
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({ email });
+      if (email === 'magadiflo@gmail.com') {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+      }
+      subscriber.next(null);
+      subscriber.complete();
+    })
+      .pipe(
+        delay(5000)
+      );
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email: string = control.value;
+  //   console.log(email);
+  //   return this.http.get<any[]>(`http://127.0.0.1:3000/usuarios?q=${email}`)
+  //     .pipe(
+  //       delay(2500), // Simulando retardo de 2.5 seg. para ver el estado del formulario
+  //       map(resp => {
+  //         return resp.length === 0 ? null : { emailTomado: true }
+  //       }),
+  //     );
+  // }
 
 }
