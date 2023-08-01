67a3f00fed02f171acb4d9afa889a89f981d6d6a
Martín <magadiflo@gmail.com>
2023-08-01 10:54:48 -0500

    Servicio e interfaces

---
+++ src/app/countries/interfaces/country.interfaces.ts
@@ -0,0 +1,157 @@
+export interface Country {
+  name:         Name;
+  tld:          string[];
+  cca2:         string;
+  ccn3:         string;
+  cca3:         string;
+  cioc?:        string;
+  independent:  boolean;
+  status:       Status;
+  unMember:     boolean;
+  currencies:   { [key: string]: Currency };
+  idd:          Idd;
+  capital:      string[];
+  altSpellings: string[];
+  region:       Region;
+  subregion:    Subregion;
+  languages:    Languages;
+  translations: { [key: string]: Translation };
+  latlng:       number[];
+  landlocked:   boolean;
+  borders?:     string[];
+  area:         number;
+  demonyms:     Demonyms;
+  flag:         string;
+  maps:         Maps;
+  population:   number;
+  gini?:        { [key: string]: number };
+  fifa?:        string;
+  car:          Car;
+  timezones:    string[];
+  continents:   Continent[];
+  flags:        Flags;
+  coatOfArms:   CoatOfArms;
+  startOfWeek:  StartOfWeek;
+  capitalInfo:  CapitalInfo;
+  postalCode?:  PostalCode;
+}
+
+export interface CapitalInfo {
+  latlng?: number[];
+}
+
+export interface Car {
+  signs?: string[];
+  side:   Side;
+}
+
+export enum Side {
+  Left = "left",
+  Right = "right",
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export enum Continent {
+  NorthAmerica = "North America",
+  Oceania = "Oceania",
+  SouthAmerica = "South America",
+}
+
+export interface Currency {
+  name:   string;
+  symbol: string;
+}
+
+export interface Demonyms {
+  eng:  EngClass;
+  fra?: EngClass;
+}
+
+export interface EngClass {
+  f: string;
+  m: string;
+}
+
+export interface Flags {
+  png:  string;
+  svg:  string;
+  alt?: string;
+}
+
+export interface Idd {
+  root:     string;
+  suffixes: string[];
+}
+
+export interface Languages {
+  aym?: string;
+  que?: string;
+  spa?: SPA;
+  eng?: EngEnum;
+  fra?: string;
+  grn?: string;
+  nld?: string;
+  pap?: string;
+  por?: string;
+  hat?: string;
+  kal?: string;
+  jam?: string;
+  bjz?: string;
+}
+
+export enum EngEnum {
+  English = "English",
+}
+
+export enum SPA {
+  Spanish = "Spanish",
+}
+
+export interface Maps {
+  googleMaps:     string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common:     string;
+  official:   string;
+  nativeName: { [key: string]: Translation };
+}
+
+export interface Translation {
+  official: string;
+  common:   string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex?: string;
+}
+
+export enum Region {
+  Africa    = 'Africa',
+  Americas  = 'Americas',
+  Asia      = 'Asia',
+  Europe    = 'Europe',
+  Oceania   = 'Oceania',
+}
+
+export enum StartOfWeek {
+  Monday = "monday",
+  Sunday = "sunday",
+}
+
+export enum Status {
+  OfficiallyAssigned = "officially-assigned",
+}
+
+export enum Subregion {
+  Caribbean = "Caribbean",
+  CentralAmerica = "Central America",
+  NorthAmerica = "North America",
+  SouthAmerica = "South America",
+}
