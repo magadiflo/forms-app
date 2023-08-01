412a1256799981bcfe20079f808be1c029176b42
Martín <magadiflo@gmail.com>
2023-08-01 10:01:55 -0500

    Estructura de directorios para Selectores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     path: 'reactive',
     loadChildren: () => import('./reactive/reactive.module').then(m => m.ReactiveModule),
   },
+  {
+    path: 'countries',
+    loadChildren: () => import('./countries/countries.module').then(m => m.CountriesModule),
+  },
   { path: '**', redirectTo: 'reactive', },
 ];
 
